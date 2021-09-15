c20b4f38f71792922c76d797db6526fd00d8378b
Chad <chad.buntrakulsuk@gmail.com>
2021-09-14 21:05:46 -0700

    -added students page

--- public/script.d573be0b.js
+++ public/script.d573be0b.js
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12479" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5113" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- sass/js/script.js
+++ sass/js/script.js
@@ -4,14 +4,14 @@ const navLinks = document.querySelector('#nav-link');
 hamBtn.addEventListener("click", function() {
 
   if (navLinks.classList.contains('navbar__links__open')) {
-    navLinks.classList.remove('navbar__links__open')
+    navLinks.classList.remove('navbar__links__open');
   } else {
-    navLinks.classList.add('navbar__links__open')
+    navLinks.classList.add('navbar__links__open');
   }
   });
 
 window.addEventListener("resize", () => {
     if (window.innerWidth >= 786) {
-      navLinks.classList.remove('navbar__links__open')
+      navLinks.classList.remove('navbar__links__open');
     }
 });
